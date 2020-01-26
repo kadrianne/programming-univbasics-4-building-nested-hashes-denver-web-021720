858784969ad7afe9f4749b256c2ad13684713537
Kristine Du <kristine.a.du@gmail.com>
2020-01-26 05:21:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 def base_hash
-  hash = {key: value}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	{key: "value"}
 end
 
 def monopoly_with_second_tier
